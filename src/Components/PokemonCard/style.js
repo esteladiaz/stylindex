008c3ed9af85edf1b857ce6e122ef2b1d81a59e7
Estela Diaz <diazsanchez.estela@gmail.com>
2019-04-24 16:35:18 -0700

    rearrange pokemon card styles, style cards

--- src/Components/PokemonCard/style.js
+++ src/Components/PokemonCard/style.js
@@ -1,67 +1,6 @@
 import styled, { css } from 'styled-components'
 import { breakpoints, colors } from '../../variables'
 
-export const TypeColor = styled.div.attrs(({ pokemonType }) => {
-  let typeColor = ''
-  let pokemonTypes = new Set([])
-
-  // let pokemonTypes = new Set(
-  //   [].concat.apply(
-  //     [],
-  //     res.departments.map(department => {
-  //       return department.jobs
-  //     })
-  //   )
-  // )
-
-  // put types into single unique array of type values, then use array[value]
-  // pokemonType === type ? (
-  //  `${type.toLowerCase.()}`
-  // expand thought
-  // )
-  console.log(typeof pokemonType, '<====string')
-  pokemonType === 'Bug' ? typeColor = colors.bugGreen
-  : pokemonType === 'Dragon' ? typeColor = colors.dragonPurple
-  : pokemonType === 'Electric' ? typeColor = colors.electricYellow
-  : pokemonType === 'Fairy' ? typeColor = colors.fairyPink
-  : pokemonType === 'Fighting' ? typeColor = colors.fightingBeige
-  : pokemonType === 'Fire' ? typeColor = colors.fireOrange
-  : pokemonType === 'Flying' ? typeColor = colors.flyOnTheWall
-  : pokemonType === 'Ghost' ? typeColor = colors.ghostGreen
-  : pokemonType === 'Grass' ? typeColor = colors.grassGreen
-  : pokemonType === 'Ground' ? typeColor = colors.groundBrown
-  : pokemonType === 'Ice' ? typeColor = colors.iceBrown
-  : pokemonType === 'Normal' ? typeColor = colors.normalGrey
-  : pokemonType === 'Poison' ? typeColor = colors.poisonVenom
-  : pokemonType === 'Psychic' ? typeColor = colors.psychicYellow
-  : pokemonType === 'Rock' ? typeColor = colors.rockyRoad
-  : pokemonType === 'Steel' ? typeColor = colors.steelyBlue
-  : pokemonType === 'Water' ? typeColor = colors.waterBlue
-  : typeColor = colors.background
-
-  return {typeColor}
-})`
-  background-color: ${props => props.typeColor};
-  border-radius: 1rem;
-  box-shadow: rgba(0,0,0,0.05) 0px 1px 0px;
-  color: #fff;
-  font-size: 0.875rem;
-  font-weight: 600;
-  height: 20px;
-  letter-spacing: 1px;
-  padding: 0.125rem 0.25rem;
-  &:nth-child(even) {
-    margin-left: 0.25rem;
-  }
-`
-
-
-
-export const TypeColorContainer = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-`
-
 const pokemonCardBreakpoints = css`
   @media (min-width: ${breakpoints.desktopMin}) {
     border: 1px solid ${colors.lightGrey};
@@ -97,7 +36,6 @@ const pokemonCardBreakpoints = css`
     width: 100%;
   }
 `
-
 export const PokemonCard = styled.a`
   ${pokemonCardBreakpoints}
   border-radius: 5px;
@@ -105,20 +43,88 @@ export const PokemonCard = styled.a`
   display: flex;
   flex-flow: column wrap;
   justify-content: space-between;
+  overflow: hidden;
   padding: 1rem;
   transition: transform 0.2s ease;
   &:hover {
     transform: translateY(-4px);
   }
-`
-
-export const PokemonCardImage = styled.div`
-  height: auto;
-  width: 180px;
-  img {
-    width: 100%;
+  h2, h3, p {
+    margin: 0;
   }
 `
 
-export const PokemonCardInformation = styled.div`
+export const PokemonCardHeaderInfo = styled.div`
+  align-items: center;
+  border-bottom: 1px solid ${colors.lightGrey};
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  padding-bottom: 1rem;
+`
+
+export const PokemonImageAndInfo = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  height: 120px;
+  margin-top: 1rem;
+  width: 100%;
+  img {
+    height: 40%;
+    left: 20%;
+    position: relative;
+  }
+`
+
+export const PokemonImageContainer = styled.div`
+  width: 50%;
+`
+
+export const PokemonCardInformation = styled.div``
+
+export const TypeColorContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  position: relative;
+  text-align: center;
+  width: 50%;
+`
+
+export const TypeColor = styled.div.attrs(({ pokemonType }) => {
+  let typeColor = ''
+  pokemonType === 'Bug' ? typeColor = colors.bugGreen
+  : pokemonType === 'Dragon' ? typeColor = colors.dragonPurple
+  : pokemonType === 'Electric' ? typeColor = colors.electricYellow
+  : pokemonType === 'Fairy' ? typeColor = colors.fairyPink
+  : pokemonType === 'Fighting' ? typeColor = colors.fightingBeige
+  : pokemonType === 'Fire' ? typeColor = colors.fireOrange
+  : pokemonType === 'Flying' ? typeColor = colors.flyOnTheWall
+  : pokemonType === 'Ghost' ? typeColor = colors.ghostGreen
+  : pokemonType === 'Grass' ? typeColor = colors.grassGreen
+  : pokemonType === 'Ground' ? typeColor = colors.groundBrown
+  : pokemonType === 'Ice' ? typeColor = colors.iceBrown
+  : pokemonType === 'Normal' ? typeColor = colors.normalGrey
+  : pokemonType === 'Poison' ? typeColor = colors.poisonVenom
+  : pokemonType === 'Psychic' ? typeColor = colors.psychicYellow
+  : pokemonType === 'Rock' ? typeColor = colors.rockyRoad
+  : pokemonType === 'Steel' ? typeColor = colors.steelyBlue
+  : pokemonType === 'Water' ? typeColor = colors.waterBlue
+  : typeColor = colors.background
+
+  return {typeColor}
+})`
+  background-color: ${props => props.typeColor};
+  border-radius: 1rem;
+  box-shadow: rgba(0,0,0,0.05) 0px 1px 0px;
+  color: #fff;
+  font-size: 0.875rem;
+  font-weight: 600;
+  height: 20px;
+  letter-spacing: 1px;
+  padding: 0.125rem 0.25rem;
+  position: relative;
+  right: 0;
+  &:nth-child(even) {
+    margin-left: 0.25rem;
+  }
 `
