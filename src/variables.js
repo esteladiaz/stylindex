9daf72dd08fec5e3fc352f01ea30acd4f050986f
Estela Diaz <diazsanchez.estela@gmail.com>
2019-04-23 17:39:57 -0700

    add variables for colors, breakpoints

---
+++ src/variables.js
@@ -0,0 +1,20 @@
+export const breakpoints = {
+  desktopMin: '1101px',
+  desktopXsMax: '1100px',
+  desktopXsMin: '1024px',
+  tabletMax: '1023px',
+  tabletMin: '600px',
+  mobileMax:'599px',
+  mobileXsMax: '350px'
+}
+
+export const colors = {
+  background: '#fff',
+  body: '#383838',
+  bugGreen: '#8ac977',
+  fireOrange: '#f4b186',
+  grassGreen: '#5b986f',
+  waterBlue: '#5ca5b9',
+  pokemonBlue: '#2f6db5',
+  pokemonYellow: '#f9cb33'
+}
