45a056cdfc5651ae723b9c9462e38f558b5d59d9
Estela Diaz <diazsanchez.estela@gmail.com>
2019-04-24 18:13:43 -0700

    add typecolors to modal

--- src/Components/PokemonCard/index.js
+++ src/Components/PokemonCard/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import {
   PokemonCard,
-  PokemonCardImage,
+  PokemonCardHeaderInfo,
   PokemonCardInformation,
+  PokemonImageAndInfo,
+  PokemonImageContainer,
   TypeColor,
   TypeColorContainer
 } from './style'
@@ -12,18 +14,22 @@ export default function(props) {
   const { onClick } = props
   return (
     <PokemonCard onClick={onClick}>
+      <PokemonCardHeaderInfo>
+        <h2 title={`id: ${id}`}>{name}</h2>
+        <h3>{number}</h3>
+      </PokemonCardHeaderInfo>
+      <PokemonImageAndInfo>
+        <PokemonCardInformation>
+          <p><strong>Max HP:</strong> {maxHP}</p>
+          <p><strong>Max CP:</strong> {maxCP}</p>
+        </PokemonCardInformation>
+        <PokemonImageContainer>
+          <img src={image} alt={name} />
+        </PokemonImageContainer>
+      </PokemonImageAndInfo>
       <TypeColorContainer>
         {types.map((type, idx) => <TypeColor key={idx} pokemonType={type}>{type}</TypeColor>)}
       </TypeColorContainer>
-      <PokemonCardImage>
-      <img src={image} alt={name} />
-      </PokemonCardImage>
-      <PokemonCardInformation>
-        <h2 title={`id: ${id}`}>{name}</h2>
-        <h3>{number}</h3>
-        <p><strong>Max HP:</strong> {maxHP}</p>
-        <p><strong>Max CP:</strong> {maxCP}</p>
-      </PokemonCardInformation>
     </PokemonCard>
   )
 }
