a1cce19bb1c8a008b9707ef6b9ebdc212b9eae43
Estela Diaz <diazsanchez.estela@gmail.com>
2019-04-24 18:12:56 -0700

    add modal styles

--- src/Components/Pokemon/style.js
+++ src/Components/Pokemon/style.js
@@ -1,7 +1,54 @@
-import styled from 'styled-components'
-import { breakpoints } from '../../variables'
+import styled, { createGlobalStyle } from 'styled-components'
+import { breakpoints, colors } from '../../variables'
+import Modal from 'styled-react-modal'
 
-export const Container = styled.div`
+export const GlobalStyle = createGlobalStyle`
+  body {
+    color: ${colors.body};
+    margin: 0;
+    padding: 0;
+    font-family: "Roboto", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
+export const HeaderText = styled.div`
+  margin-bottom: 2rem;
+  text-align: center;
+  p {
+    margin: 0;
+  }
+`
+
+export const Logo = styled.img`
+  @media (min-width:${breakpoints.tabletMin}) {
+    width: 400px;
+    margin-bottom: 1rem;
+  })
+`
+
+export const SelectedPokemonWrapper = Modal.styled`
+  background: white;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 0px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 1rem;
+  @media (max-width: ${breakpoints.mobileMax}) {
+    width: 80%;
+  }
+`
+
+export const PokemonCardWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: start;
+`
+
+export const PageContainer = styled.div`
   @media (min-width: ${breakpoints.desktopMin}) {
     margin: 0 3rem;
   }
@@ -11,10 +58,4 @@ export const Container = styled.div`
   @media (max-width: ${breakpoints.mobileMax}) {
     margin: 0 0.5rem;
   }
-`
-
-export const PokemonCardWrapper = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-between;
 `
